d9f7daf1959c16593fbc39731b41b0f6d77e7b1a
cherniak-artem <amacenko22@ukr.net>
2026-09-17 21:32:34 +0300

    All tasks complited

---
+++ src/lab_1/task_1.java
@@ -0,0 +1,25 @@
+package lab_1;
+
+public class task_1 {
+
+    // Константи
+    public static final double A = -1.49;
+    public static final double B = 23.4;
+    public static final double C = 1.23;
+    public static final double D = 2.542;
+
+    // Функція для обчислення y
+    public static double calculateY(double a, double b, double c, double d) {
+        return Math.sqrt(
+                Math.abs(Math.sin(a) - (4 * Math.log(b)) / Math.pow(c, d))
+        );
+    }
+
+    public static void main(String[] args) {
+
+        double y = calculateY(A, B, C, D);
+
+        System.out.println("Розв'язок задачі:");
+        System.out.println("y = " + y);
+    }
+}
---
+++ src/lab_1/task_3.java
@@ -0,0 +1,23 @@
+package lab_1;
+
+public class task_3 {
+
+    // Вхідні дані як константи
+    public static final double A = -2.54;
+    public static final double B = 1.23;
+    public static final double C = -2.14;
+    public static final double D = -0.23;
+
+    // Функція для обчислення y
+    public static double calculateY() {
+        return 3 * (Math.log(Math.abs(A / B)) + Math.sqrt(Math.cos(C) + Math.exp(D))
+        );
+    }
+
+    public static void main(String[] args) {
+
+        double y = calculateY();
+
+        System.out.printf("y = %.4f%n", y);
+    }
+}
